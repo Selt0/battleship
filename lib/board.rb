ab0e3c8d1934c19bfccfb278d248259f1b138247
Selt0 <chewedgum12@gmail.com>
2020-05-03 22:54:25 -0500

    Add hidden ships method. Print both board with and without ships revealed

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,7 @@
 class Board
   
   attr_reader :grid, :length, :ships
+ 
   def initialize(num)
     @grid = Array.new(num) { Array.new(num, '_')}
     @ships = num * num * 0.25
@@ -16,7 +17,7 @@ class Board
     x, y = pos
     grid[x][y] = value
   end
-  
+
   def print_grid(grid)
     puts "  #{(0..length - 1).to_a.join(' ')}"
     grid.each_with_index do |row, i|
@@ -34,5 +35,25 @@ class Board
       total_ships += 1
     end
     nil
+  end
+
+  def hidden_ships
+    grid.map do |row|
+      row.map do |ele|
+        if ele == :S
+          ele = '_'
+        else
+          ele
+        end
+      end
+    end
+  end
+
+  def print
+    print_grid(self.hidden_ships)
+  end
+
+  def reveal
+    print_grid(grid)
   end
 end
