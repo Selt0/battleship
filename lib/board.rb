82d3d5f65f741bab295e8892ce6d9e0b5f0ee8b9
Selt0 <chewedgum12@gmail.com>
2020-05-03 22:41:08 -0500

    Add random_ships method

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,38 @@
 class Board
   
-  attr_reader :grid, :size
-  def initialize(size)
-    @grid = Array.new(size) { Array.new(size, '_')}
-    @size = size
+  attr_reader :grid, :length, :ships
+  def initialize(num)
+    @grid = Array.new(num) { Array.new(num, '_')}
+    @ships = num * num * 0.25
+    @length = num
+  end
+
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos
+    grid[x][y] = value
   end
   
   def print_grid(grid)
-    puts "  #{(0..size - 1).to_a.join(' ')}"
+    puts "  #{(0..length - 1).to_a.join(' ')}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(' ')}"
     end
+  end
+
+  def place_random_ships
+    total_ships = 0
+
+    while total_ships < ships
+      rand_pos = Array.new(2) { rand(length) }
+      next if self[rand_pos] == :S
+      self[rand_pos] = :S
+      total_ships += 1
+    end
+    nil
   end
 end
