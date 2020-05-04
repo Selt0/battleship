7c2e8439f595991d9412392b8af02776dc80e69d
Selt0 <chewedgum12@gmail.com>
2020-05-04 08:49:09 -0500

    Add methods to start game and define turns, win, and lose

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -3,7 +3,45 @@ require_relative "player"
 
 class Battleship
 
-  def initialize
-    
+  attr_reader :player, :board
+
+  def initialize(size)
+    @board = Board.new(size)
+    @player = Player.new
+    @remaining_guesses = board.size ** 2 / 2
+  end
+
+  def start_game
+    board.place_random_ships
+    puts "Number of ships: #{board.ships}"
+    board.print
+  end
+
+  def game_over?
+    lose? || win?
+  end
+
+  def lose?
+   if @remaining_guesses == 0
+    puts "You lose!"
+    return true
+   end
+   false
+  end
+
+  def win?
+    if board.ships == 0
+      puts 'You Win!'
+      return true
+    end
+    false
+  end
+
+  def turn
+    if !board.attack(player.get_move)
+      @remaining_guesses -= 1
+    end
+    board.print
+    puts "Remaining chances: #{@remaining_guesses}"
   end
 end
