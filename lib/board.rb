9c3eaceebe2c092da392e69e69f2a8aeea0dd8e3
Selt0 <chewedgum12@gmail.com>
2020-05-03 21:55:22 -0500

    Add board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,15 @@
 class Board
   
+  attr_reader :grid, :size
+  def initialize(size)
+    @grid = Array.new(size) { Array.new(size, '_')}
+    @size = size
+  end
+  
+  def print_grid(grid)
+    puts "  #{(0..size - 1).to_a.join(' ')}"
+    grid.each_with_index do |row, i|
+      puts "#{i} #{row.join(' ')}"
+    end
+  end
 end
