b3743948616cf452177398c452b3081cdade67b8
Selt0 <chewedgum12@gmail.com>
2020-05-04 08:48:24 -0500

    rename 'length' to size

--- lib/board.rb
+++ lib/board.rb
@@ -1,11 +1,11 @@
 class Board
   
-  attr_reader :grid, :length, :ships
+  attr_reader :grid, :size, :ships
  
   def initialize(num)
     @grid = Array.new(num) { Array.new(num, '_')}
     @ships = num * num * 0.25
-    @length = num
+    @size = num
   end
 
   def [](pos)
@@ -19,7 +19,7 @@ class Board
   end
 
   def print_grid(grid)
-    puts "  #{(0..length - 1).to_a.join(' ')}"
+    puts "  #{(0..size - 1).to_a.join(' ')}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(' ')}"
     end
@@ -29,7 +29,7 @@ class Board
     total_ships = 0
 
     while total_ships < ships
-      rand_pos = Array.new(2) { rand(length) }
+      rand_pos = Array.new(2) { rand(size) }
       next if self[rand_pos] == :S
       self[rand_pos] = :S
       total_ships += 1
